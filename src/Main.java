4d959bd687725a6229235182392f5afd7230f4ff
Vieness <ydvion13@gmail.com>
2022-10-08 17:22:21 +0300

    Case 3/4 done

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,24 @@
 public class Main {
     public static void main(String[] args) {
+        //#2 Case
         int typeInt = 127;
         float typeFloat = 0.27f;
         double typeDouble = 0.27;
         boolean typeBoolean = true;
         char typeChar = 'a';
-        System.out.println(typeInt + "\n"
-                + typeFloat + "\n"
-                + typeDouble + "\n"
-                + typeBoolean + "\n"
-                + typeChar);
+        System.out.println("Int: " + typeInt
+                + "\nFloat: " + typeFloat +
+                "\nDouble: " + typeDouble +
+                "\nBoolean: " + typeBoolean +
+                "\nChar: " + typeChar);
+        //#5 Case
         enum Gender {
-            male, female
+            MALE, FEMALE
         }
-        System.out.println(Gender.female + " " +Gender.male);
+        System.out.println("Gender Output: " + Gender.MALE + " " + Gender.FEMALE);
     }
-
-
-
-
 }
+// #3Case (целочисленный тип данных из 12 бит)
+//2^12 = 4096/2 = 2048 (диапазон значений в 12 битной целочисленный переменной от -2048 до 2047)
+// #4Case
+//2^4 = 16 (осчет в Char начинается с 0 и того диапазон от 0 до 15 символов)
